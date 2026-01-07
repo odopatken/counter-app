9f673b7310e0f65b0e3a7b6252863da2a5d4e6f5
odopatken <odopatken@gmail.com>
2026-01-07 21:17:44 +0100

    My first commit - creating a header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-
-import './App.css'
-
+import Header from "./Header"
+import Footer from "./Footer";
 function App() {
-  const [count, setCount] = useState(0)
-  const increment = () => {
-    setCount(count + 1);
-  }
-  const decrement = () => {
-    if (count > 0) {
-      setCount(count - 1);
-    }
-  }
-  const reset = () => setCount(0) 
 
-  return (
-    <>
-    <div>
-      <h1>ClickCounter App</h1>
-      <div className="card">
-        <button onClick={decrement}>-Decrement</button>
-        <button onClick={increment}>+Increment</button>
-        <button onClick={reset}>Reset</button>
-        <p>Count: {count}</p>
-      </div>
+    return(
+        <>
+         
+        <Header />
+        <Footer />
+       
+        </>
+        
+    );
 
-    </div>
-    </>
-  )
 }
 
 export default App
---
+++ src/Footer.tsx
@@ -0,0 +1,16 @@
+function Footer(){
+
+    return(
+
+        <footer>
+
+            <div>
+                
+                <small> &copy; {new Date().getFullYear()} Your website name </small>
+            </div>
+           
+        </footer>
+    );
+
+}
+export default Footer
